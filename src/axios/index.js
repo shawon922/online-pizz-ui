a87d5379ed4e62ce4be0dabd5af42d7d01cd0b2e
Md Mahfuzur Rahman <shawon922@gmail.com>
2020-08-14 08:56:31 +0600

    read api base url from env

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
   timeout: 30000,
   headers: { 
     "Content-type": "application/x-www-form-urlencoded", 
